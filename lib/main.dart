1c51726aa32ef129a6ef4d605a0cfd5fd3ffeac6
cv692001 <64506066+cv692001@users.noreply.github.com>
2020-10-07 21:00:18 +0530

    small change

--- lib/main.dart
+++ lib/main.dart
@@ -17,27 +17,3 @@ class BMICalculator extends StatelessWidget {
   }
 }
 
-class InputPage extends StatefulWidget {
-  @override
-  _InputPageState createState() => _InputPageState();
-}
-
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Hey Buddy'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add_to_home_screen),
-        onPressed: null,
-
-      ),
-      home: InputPage(),
-    );
-  }
-}
